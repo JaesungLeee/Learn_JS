e82b115a266f94d0b49f69ae5d12127bf3cc2580
JaesungLeee <viki0114@naver.com>
2021-01-29 23:31:42 +0900

    Lecture #2.1

---
+++ makeChromeApplication/#2. Practice/index.js
@@ -0,0 +1,16 @@
+var userInfo = {} 
+userInfo = {
+    name : "JSLEE",
+    age : 25,
+    gender : "Male",
+    isHandsome : true
+}
+
+console.log(userInfo.isHandsome)
+
+// 함수
+function sayHello(name, age) {
+    console.log("Hello", name, age)
+}
+
+sayHello("JSLEE", 15) // 여러 인자를 포함할 수 있음.
